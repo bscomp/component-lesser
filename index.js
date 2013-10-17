83fac159c7d049672597359eec8584e0f43553de
Eldar Djafarov <djkojb@gmail.com>
2013-10-17 06:40:14 -0300

    fix a bug

--- index.js
+++ index.js
@@ -17,14 +17,17 @@ module.exports = function (builder, options) {
 	options = options || {};
   var bindex = 0;
   var lessImport = [];
-  builder.on('dependency', function(b){
+  builder.on('dependency', depHandler);
+
+  function depHandler(b){
     b.dep = true;
+    b.on('dependency', depHandler);
     b.config.styles.forEach(function(file){
       if(isLess(file) && !~lessImport.indexOf(b.path(file))){
         lessImport.push(b.path(file));
       }
     });
-  })
+  }
   builder.hook('before styles', function (builder, callback) {
     if(!builder.root) {
       builder.config.styles.forEach(function(file){
@@ -33,7 +36,6 @@ module.exports = function (builder, options) {
       return callback();
     }
     builder.config.styles.slice().forEach(function(file){
-      console.log(isLess(file) && !~lessImport.indexOf(builder.path(file)), file);
       if(isLess(file) && !~lessImport.indexOf(builder.path(file))){
         lessImport.push(builder.path(file));
       }
@@ -43,9 +45,9 @@ module.exports = function (builder, options) {
     lessImport.forEach(function(file){
       importsLess+='@import "'+file +'";\n';
     });
-		
     var parser = new less.Parser(options.env || {});
 		var cssConfig = options.cssConfig || {};
+    setTimeout(function(){
     parser.parse(importsLess, function (error, tree) {
       if (error) {
 				return cb(error);
@@ -55,6 +57,7 @@ module.exports = function (builder, options) {
 			builder.addFile('styles', "compiledLess", css);
 		  callback()
     });
+    },1000);
 	});
 };
 
